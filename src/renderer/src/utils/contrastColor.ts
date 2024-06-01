5b6a06f7798176997ca57ac00ab4e4a952d83fca
ttngmai <frozenfruit24@gmail.com>
2024-06-01 17:31:05 +0900

    밝기 판단 유틸 함수 수정

--- src/renderer/src/utils/contrastColor.ts
+++ src/renderer/src/utils/contrastColor.ts
@@ -4,6 +4,14 @@ function isLight(color: string): boolean {
   const getRGB = (hex: string): TRGB => {
     if (hex.startsWith('#')) {
       hex = hex.substring(1)
+
+      if (hex.length === 3) {
+        hex = hex
+          .split('')
+          .map((char) => char + char)
+          .join('')
+      }
+
       const bigint = parseInt(hex, 16)
       const r = (bigint >> 16) & 255
       const g = (bigint >> 8) & 255
